37344ec7c80799e217b7bf16f90a5ef108187a87
Avenhadeproject <876acraft@gmail.com>
2025-05-08 07:09:14 +0200

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
-    // le menu sur tel
-    document.getElementById('menu-toggle').addEventListener('click', function() {
+document.getElementById('menu-toggle').addEventListener('click', function() {
       this.classList.toggle('active');
       const nav = document.getElementById('nav-links');
       nav.classList.toggle('active');
     });
 
-    // fermer le menu quand tu click a coté
-    window.addEventListener('click', function(event) {
+window.addEventListener('click', function(event) {
       const nav = document.getElementById('nav-links');
       const menuToggle = document.getElementById('menu-toggle');
       if (!menuToggle.contains(event.target) && !nav.contains(event.target)) {
@@ -16,8 +13,7 @@
       }
     });
 
-    // fermer menu quand click sur un titre
-    const menuLinks = document.querySelectorAll('nav a');
+const menuLinks = document.querySelectorAll('nav a');
     menuLinks.forEach(link => {
       link.addEventListener('click', function() {
         document.getElementById('menu-toggle').classList.remove('active');
@@ -25,17 +21,7 @@
       });
     });
 
-  //  animation quand scroll legacy
-  //  const sections = document.querySelectorAll('.fade-in');
-  //  window.addEventListener('scroll', () => {
-  //    sections.forEach(section => {
-  //      if (section.getBoundingClientRect().top < window.innerHeight) {
-  //        section.classList.add('visible');
-  //      }
-  //     });
-  //   });
-
-const sections = document.querySelectorAll('.fade-in');
+  const sections = document.querySelectorAll('.fade-in');
 
 function checkVisibility() {
   sections.forEach(section => {
@@ -45,9 +31,5 @@ function checkVisibility() {
     }
   });
 }
-
-// Lancer une première vérif au chargement
 window.addEventListener('DOMContentLoaded', checkVisibility);
-
-// Puis continuer à checker au scroll
 window.addEventListener('scroll', checkVisibility);
